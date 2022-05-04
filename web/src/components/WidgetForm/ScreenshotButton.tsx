8b3dbe5e2e0b4d135600d51ae2b247ca0a80a9bc
Eric Batista <klose.eric31@gmail.com>
2022-05-04 18:31:24 -0300

    fix: feedback screenshot image on Camera icon

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -31,7 +31,7 @@ export function ScreenshotButton({
         className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
         onClick={() => onScreenshotTook(null)}
         style={{
-          backgroundImage: `url($screenshot)`,
+          backgroundImage: `url(${screenshot})`,
           backgroundPosition: 'right bottom',
           backgroundSize: 180,
         }}
